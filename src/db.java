bd42f13c91e5a288266121b851b406b649b21b82
Manoj Karki <nrdygx@gmail.com>
2023-01-23 14:21:38 +0545

    cleanup(db/java): remove unused imports

--- src/db.java
+++ src/db.java
@@ -1,10 +1,6 @@
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import javax.swing.JOptionPane;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
