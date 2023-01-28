83b57ad202302037f6609886c15c24f99bde5b82
Manoj Karki <nrdygx@gmail.com>
2023-01-28 21:44:49 +0545

    refactor(form/java): renamed the file to "InstructorPortal"

--- src/InstructorPortal.java
+++ src/InstructorPortal.java
@@ -7,12 +7,12 @@
  *
  * @author manoj
  */
-public class TeacherPortal extends javax.swing.JFrame {
+public class InstructorPortal extends javax.swing.JFrame {
 
     /**
      * Creates new form TeacherPortal
      */
-    public TeacherPortal() {
+    public InstructorPortal() {
         initComponents();
     }
 
@@ -57,20 +57,21 @@ public class TeacherPortal extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TeacherPortal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(InstructorPortal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TeacherPortal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(InstructorPortal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TeacherPortal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(InstructorPortal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TeacherPortal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(InstructorPortal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new TeacherPortal().setVisible(true);
+                new InstructorPortal().setVisible(true);
             }
         });
     }
