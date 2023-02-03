9aaf56ad524de094cee47eefff3af6f72b71fad7
Manoj Karki <nrdygx@gmail.com>
2023-02-03 11:50:45 +0545

    refactor(sql): fix column datatype and added more tables.

--- import.sql
+++ import.sql
@@ -6,7 +6,7 @@ create table `instructors` (
     `firstname` varchar(50) null,
     `middlename` varchar(50) default "none",
     `lastname` varchar(50) null,
-    `gender` int(1) default 0,
+    `gender` varchar(50) default "none",
     `phone` varchar(50) null,
     `address` varchar(255) null,
     `username` varchar(50) null,
@@ -15,15 +15,26 @@ create table `instructors` (
     `updatedOn` datetime default current_timestamp() on update current_timestamp(),
     primary key(id)
 );
-
 create table `courses` (
     `id` int(11) auto_increment,
+    `name` varchar(255) default "none",
+    `year` int(11) default 1,
+    `isActive` int(11) default 0,
+    `updatedOn` datetime default current_timestamp() on update current_timestamp(),
+    primary key (id)
+);
+
+create table `modules` (
+    `id` int(11) auto_increment,
+    `course` int,
     `code` varchar(20) default "none",
-    `module` varchar(255) default "none",
+    `name` varchar(255) default "none",
+    `year` int(11) default 1,
     `instructor` int,
     `updatedOn` datetime default current_timestamp() on update current_timestamp(),
     primary key (id),
-    Foreign Key (instructor) REFERENCES instructors(id)
+    Foreign Key (course) REFERENCES courses(id),
+    Foreign Key(instructor) REFERENCES instructors(id)
 );
 
 create table `students` (
@@ -31,7 +42,7 @@ create table `students` (
     `firstname` varchar(50) null,
     `middlename` varchar(50) default "none",
     `lastname` varchar(50) null,
-    `gender` int(1) default 0,
+    `gender` varchar(50) default "none",
     `phone` varchar(50) null,
     `address` varchar(255) null,
     `username` varchar(50) null,
@@ -41,12 +52,24 @@ create table `students` (
     primary key(id)
 );
 
+create table `enrolled_modules` (
+    `id` int(11) auto_increment,
+    `module` int,
+    `student` int,
+    `instructor` int,
+    `updatedOn` datetime default current_timestamp() on update current_timestamp(),
+    primary key(id),
+    Foreign Key(module) REFERENCES modules(id),
+    Foreign Key(instructor) REFERENCES modules(instructor),
+    Foreign Key(student) REFERENCES students(id)
+);
+
 create table `admins` (
     `id` int(11) auto_increment,
     `firstname` varchar(50) null,
     `middlename` varchar(50) default "none",
     `lastname` varchar(50) null,
-    `gender` int(1) default 0,
+    `gender` varchar(50) default "none",
     `phone` varchar(50) null,
     `address` varchar(255) null,
     `username` varchar(50) null,
